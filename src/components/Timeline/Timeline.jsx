099c59b3fccdda9d0aff66aee558c95f6686d012
Gargee <gargeedhale07@gmail.com>
2026-02-03 13:43:16 +0530

    Update timeline events with detailed information

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -3,20 +3,20 @@ import "./Timeline.css";
 import timeline from "../../assets/timeline.mp4";
 
 const data = [
-  { event: "Inauguration", date: "Coming Soon" },
-  { event: "Speaker Session", date: "Coming Soon" },
-  { event: "Main Quest - 1", date: "Coming Soon" },
-  { event: "Side Quest - 1", date: "Coming Soon" },
-  { event: "Side Quest - 2", date: "Coming Soon" },
-  { event: "Main Quest - 2", date: "Coming Soon" },
-  { event: "Quest for Seniors", date: "Coming Soon" },
-  { event: "Quest for our juniors", date: "Coming Soon" },
-  { event: "MUN", date: "Coming Soon" },
-  { event: "Side Quest - 3", date: "Coming Soon" },
-  { event: "Side Quest - 4", date: "Coming Soon" },
-  { event: "Main Quest - 3", date: "Coming Soon" },
-  { event: "Grand Finale", date: "Coming Soon" },
-  { event: "Valedictory and Prize Distribution", date: "Coming Soon" }
+  { event: "The Hindu Quiz", venue: "HIRA Hall", date: "4th February", time: "09:00 AM - 02:00 PM" },
+  { event: "Tube Trolls 5.0 (Prelims)", venue: "HIRA Hall", date: "4th February", time: "05:00 PM - 06:30 PM" },
+  { event: "Inauguration",venue: "HIRA Hall", date: "5th February", time: "10:00 AM" },
+  { event: "Speaker Session",venue: "HIRA Hall", date: "5th February", time: "11:00 AM - 01:00 PM" },
+  { event: "Storyforge Championship", venue: "IT Classroom", date: "5th February", time: "05:00 PM - 06:30 PM" },
+  { event: "MUN", venue: "Venue to be disclosed soon", date: "6th February", time: "09:00 AM - 07:00 PM" },
+  { event: "Essay Writing", venue: "IT Classroom", date: "7th February", time: "09:00 AM - 10:30 AM" },
+  { event: "The Literary Marathon", venue: "F36/F38 Classroom", date: "7th February", time: "10:30 AM - 02:00 PM"},
+  { event: "Debate", venue: "D2 Classroom", date: "7th February", time: "03:00 PM - 05:00 PM" },
+  { event: "Poetry Slam R-1", venue: "HIRA Hall", date: "7th February", time: "05:00 PM - 07:00 PM" },
+  { event: "Tube Trolls 5.0 (Finals)", venue: "HIRA Hall", date: "8th February", time: "09:30 AM - 11:30 AM" },
+  { event: "Poetry Slam (Showdown)", venue: "HIRA Hall", date: "8th February", time: "11:30 AM - 01:30 PM" },
+  { event: "Grand Finale AIRCRASH ",venue: "HIRA Hall", date: "8th February", time: "02:30 PM Onwards" },
+  { event: "Valedictory and Prize Distribution", venue: "HIRA Hall", date: "8th February", time: "05:00 PM Onwards" },
 ];
 
 const Timeline = () => {
@@ -79,9 +79,14 @@ const Timeline = () => {
               ref={(el) => (itemsRef.current[index] = el)}
               className={`milestone ${index % 2 === 0 ? "left" : "right"}`}
             >
-              <h3>{item.event}</h3>
-              <p>{item.date}</p>
-            </div>
+               <h3>{item.event}</h3>
+               <p className="venue"><h4>{item.venue}</h4></p>
+               <p className="date">{item.date}</p>
+
+      {item.time && (
+               <p className="time">{item.time}</p>
+            )}
+          </div>
           ))}
         </div>
       </div>
