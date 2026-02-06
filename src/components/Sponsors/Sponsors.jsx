85e18552f497b61e7e0764d28b2418fb23254aba
Gargee <gargeedhale07@gmail.com>
2026-02-06 08:50:25 +0530

    Update Sponsors.jsx

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Sponsors.css";
 import sponsorbg from "../../assets/sponsorbg.mp4";
+import s1 from "../../assets/images/sponsor1.jpg";
+import s2 from "../../assets/images/sponsor2.jpg";
 
-const Sponsors = () => {
+const Sponsors = [
+  { img: s1, name: "Main Sponsor" },
+  { img: s2, name: "Co Sponsor" },
+];
+
+const SponsorsComponent = () => {
   return (
     <section id="sponsors" className="sponsors">
               <video
@@ -18,10 +25,16 @@ const Sponsors = () => {
               <div className="sponsors-overlay"> </div>
              <div className="sponsors-content">
       <h2>Our Sponsors</h2>
-      <p>Coming Soon</p>
+      <div className="sponsor-grid">
+        {Sponsors.map((sp, i) => (
+        <div key={i} className="sponsor-card">
+        <img src={sp.img} alt={sp.name} />
+      </div>
+      ))}
+      </div>
       </div>
     </section>
   );
 };
 
-export default Sponsors;
+export default SponsorsComponent;
